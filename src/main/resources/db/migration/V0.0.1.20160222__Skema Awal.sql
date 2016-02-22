534e7b1e13d654f0685fce2cd46867d376d2c4ef
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-22 10:47:07 +0700

    konfigurasi database

---
+++ src/main/resources/db/migration/V0.0.1.20160222__Skema Awal.sql
@@ -0,0 +1,7 @@
+-- tabel Product --
+create table product (
+    id varchar(32) primary key,
+    code varchar(10) not null unique,
+    name varchar(255) not null,
+    price decimal(19,2) not null
+) Engine=InnoDB;
