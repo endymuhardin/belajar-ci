e056e5fe62c79f59d0da5b2875bdccfc3feea8d5
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-22 09:04:26 +0700

    template project

---
+++ src/main/java/com/muhardin/endy/belajar/BelajarCiApplication.java
@@ -0,0 +1,12 @@
+package com.muhardin.endy.belajar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarCiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/muhardin/endy/belajar/BelajarCiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.muhardin.endy.belajar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BelajarCiApplication.class)
+@WebAppConfiguration
+public class BelajarCiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
