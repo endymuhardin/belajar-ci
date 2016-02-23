f870101a9c2c7360eb752fd05c067bc49da1e25b
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-23 17:03:48 +0700

    script migrasi untuk postgresql

---
+++ src/main/resources/db/migration/postgresql/V0.0.1.20160222__Skema Awal.sql
@@ -0,0 +1,7 @@
+-- tabel Product --
+create table product (
+    id varchar(36) primary key,
+    code varchar(10) not null unique,
+    name varchar(255) not null,
+    price decimal(19,2) not null
+);
