23fdbefd5857892673c592eef1dbc195b2edbfe9
Endy Muhardin <endy.muhardin@gmail.com>
2017-04-05 14:40:18 +0700

    enable spring session jdbc

--- src/main/java/com/muhardin/endy/belajar/BelajarCiApplication.java
+++ src/main/java/com/muhardin/endy/belajar/BelajarCiApplication.java
@@ -2,10 +2,11 @@ package com.muhardin.endy.belajar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
+@EnableJdbcHttpSession
 public class BelajarCiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BelajarCiApplication.class, args);
 	}
