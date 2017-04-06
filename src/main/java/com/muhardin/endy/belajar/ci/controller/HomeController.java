cf416b72c6e72f662e9dd8782f4ad4de236ab941
Endy Muhardin <endy.muhardin@gmail.com>
2017-04-06 09:56:53 +0700

    display git commit info and environment variables

--- src/main/java/com/muhardin/endy/belajar/ci/controller/HomeController.java
+++ src/main/java/com/muhardin/endy/belajar/ci/controller/HomeController.java
@@ -1,19 +1,24 @@
 package com.muhardin.endy.belajar.ci.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.GitProperties;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api")
 public class HomeController {
+
+    @Autowired private Environment environment;
+    @Autowired(required = false) private GitProperties gitProperties;
 
     @GetMapping("/host")
     public Map<String, Object> hostInfo(HttpServletRequest request){
@@ -21,6 +26,12 @@ public class HomeController {
         info.put("Hostname", request.getLocalName());
         info.put("IP Address Local", request.getLocalAddr());
         info.put("Port Local", request.getLocalPort());
+        info.put("Active Profiles", environment.getActiveProfiles());
+        if (gitProperties != null){
+            info.put("Git Branch", gitProperties.getBranch());
+            info.put("Git Commit ID", gitProperties.getShortCommitId());
+            info.put("Git Commit Time", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(gitProperties.getCommitTime()));
+        }
         return info;
     }
 
